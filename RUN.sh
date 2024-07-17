efb8397420a0999e775b6560c9cea7ad9fa86d7a
Yehor <116878096+licht8@users.noreply.github.com>
2024-07-18 00:34:05 +0200

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Check if the build directory exists
 if [ ! -d "build" ]; then
     echo "Build directory does not exist. Running ./BUILD.sh..."
     ./BUILD.sh
 else
-    # Check if the bin directory exists
     if [ ! -d "build/bin" ]; then
         echo "Bin directory is missing. Please run ./BUILD.sh to create it."
         exit 1
     else
-        # Check if the executable file Compress-File exists
         if [ -f "build/bin/Compress-File" ]; then
             echo "Starting 'Compress-File'..."
             cd ./build/bin/
