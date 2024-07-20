8d278c1716154da7be0323089477234fa6dd4ee3
Yehor <116878096+licht8@users.noreply.github.com>
2024-07-20 21:22:42 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 int main() {
     int choice;
     char ifname[100], ofname[100];
+    char* filepath;
 
     printf("Choose operation (1: Compress, 2: Decompress): ");
     if (scanf("%d", &choice) != 1) {
@@ -17,30 +18,45 @@ int main() {
             fprintf(stderr, "Error reading input filename\n");
             return 1;
         }
+        filepath = find_file(ifname);
+        if (filepath == NULL) {
+            fprintf(stderr, "Input file does not exist\n");
+            return 1;
+        }
         printf("Enter output filename: ");
         if (scanf("%s", ofname) != 1) {
             fprintf(stderr, "Error reading output filename\n");
             return 1;
         }
-        if (compress(ifname, ofname) != 0) {
+        if (compress(filepath, ofname) != 0) {
             fprintf(stderr, "Compression failed\n");
+            free(filepath);
             return 1;
         }
+        free(filepath);
     } else if (choice == 2) {
         printf("Enter input filename: ");
         if (scanf("%s", ifname) != 1) {
             fprintf(stderr, "Error reading input filename\n");
             return 1;
         }
+        filepath = find_file(ifname);
+        if (filepath == NULL) {
+            fprintf(stderr, "Input file does not exist\n");
+            return 1;
+        }
         printf("Enter output filename: ");
         if (scanf("%s", ofname) != 1) {
             fprintf(stderr, "Error reading output filename\n");
+            free(filepath);
             return 1;
         }
-        if (extract(ifname, ofname) != 0) {
+        if (extract(filepath, ofname) != 0) {
             fprintf(stderr, "Decompression failed\n");
+            free(filepath);
             return 1;
         }
+        free(filepath);
     } else {
         fprintf(stderr, "Invalid choice\n");
         return 1;
